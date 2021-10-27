048e0a9d72a35a9bfecd7937f6bee1c877152985
Burak Mert Muş <burakmertmus@hotmail.com>
2021-10-27 19:50:05 +0300

    init commit

--- Program.cs
+++ Program.cs
@@ -4,19 +4,31 @@ namespace HexToDeci
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Sayı Giriniz");
-            string HexadecimalValue=Console.ReadLine();
-            Console.WriteLine(HexToDecimal(HexadecimalValue));
+
+            while(0<1){
+                Console.WriteLine("-------------");
+                Console.WriteLine("Sayı Giriniz");
+                string HexadecimalValue = Console.ReadLine();
+                Console.WriteLine("Cevap:");
+                Console.WriteLine(HexToDecimal(HexadecimalValue));
+                Console.WriteLine("-------------"); 
+
+            }
 
         }
+
         private static int HexToDecimal(string Hex)
         {
             int deci = 0,value=0;
 
             for (int i = 0; i < Hex.Length; i++)
             {
+             
+
+               
                 if (Hex[i] >= '0' && Hex[i] <= '9')
                 {
                     value = Hex[i] - 48;
@@ -29,7 +41,15 @@ namespace HexToDeci
                 {
                     value = Hex[i] - 65 + 10;
                 }
+                else
+                {
+                    
+                    Console.WriteLine("Hexadecimal olmayan karakter indisi:");
+                    return i;
+                    
+                }
                 
+
                 int a = 1;
                 for (int j = 0; j < Hex.Length - i-1; j++)
                 {
